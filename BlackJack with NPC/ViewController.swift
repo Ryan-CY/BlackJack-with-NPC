f8813bf5b0dd1616cb4094f1c16d1d62d1fff0cc
Ryan CY <phillin820109@msn.com>
2022-10-09 15:58:00 +0800

    Initial Commit

---
+++ BlackJack with NPC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackJack with NPC
+//
+//  Created by Ryan Lin on 2022/10/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
